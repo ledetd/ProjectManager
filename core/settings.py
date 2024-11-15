2caa334e7eba9f997f637ddab15ef13a05d9fa15
Dwight Ledet <dwightledet@protonmail.com>
2024-11-15 11:15:50 -0330

    sec

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,12 @@
 
 from pathlib import Path
 
+
+import os
+from decouple import config
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w07&5gwrdk==i^43uy8szu2ftmy_-izw!_-((jd!p-e5l)whj6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
