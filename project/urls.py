793e69d30956dc06cde50680d39a73abf6dff638
Dwight Ledet <dwightledet@protonmail.com>
2024-11-19 08:19:44 -0330

    boards

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, Dashboard
+from .views import Index, Dashboard, Crewboard, Wellboard
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('crewboard/', Crewboard.as_view(), name='crewboard'),
+    path('wellboard/', Wellboard.as_view(), name='wellboard'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 class Index(TemplateView):
 	template_name = 'project/index.html'
 
-class Dashboard(LoginRequiredMixin, View):
+class Dashboard( View):
 	def get(self, request):
-
-
 		return render(request, 'project/dashboard.html', {})
+	
+class Crewboard( View):
+	def get(self, request):
+		return render(request, 'crew/crewboard.html', {})
+	
+class Wellboard( View):
+	def get(self, request):
+		return render(request, 'wells/wellboard.html', {})
 
