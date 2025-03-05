417e82459ab8534025cae957ee6ab06254a7b8aa
Dwight Ledet <dwightledet@protonmail.com>
2025-03-05 11:08:08 -0330

    operations

---
+++ operation/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+from django.utils import timezone
+from django.db.models import Sum
+
+
+class Home(TemplateView):
+	template_name = 'operation/home.html'
